745166cc49a0b0ff60301c8f2b456089df7f1459
MuhammadjonAzimov <muhammadazimov921@gmail.com>
2022-01-20 20:59:01 +0500

    ozgartirish kiritildi

--- script/script.js
+++ script/script.js
@@ -5,7 +5,7 @@ menuList.style.height = "0px";
 function togglemenu() {
 
     if (menuList.style.height == "0px") {
-        menuList.style.height = "220px";
+        menuList.style.height = "210px";
     }
     else {
         menuList.style.height = "0px";
