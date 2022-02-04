638fd64becba591d872f0ff0d0346bb43c11d137
MuhammadjonAzimov <muhammadazimov921@gmail.com>
2022-02-04 22:26:01 +0500

    navigatsiya sozlandi

--- script/script.js
+++ script/script.js
@@ -11,3 +11,37 @@ function togglemenu() {
         menuList.style.height = "0px";
     }
 }
+
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+        const { latitude, longitude } = position.coords
+
+        const map = L.map('map').setView([latitude, longitude], 25);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        L.marker([41.188068, 69.195324]).addTo(map)
+            .bindPopup('ISUZU SERVISE CENTER')
+            .openPopup();
+
+        map.on("click", (event) => {
+            const { lat, lng } = event.latlng
+
+            L.marker([lat, lng])
+                .addTo(map)
+                .bindPopup(
+                    L.popup({
+                        maxWidth: 250,
+                        minWidht: 150,
+                        autoClose: false,
+                        closeOnClick: false,
+                        className: "running-popup"
+                    })
+                ).setPopupContent("MARKER")
+                .openPopup();
+
+        })
+    })
+}
